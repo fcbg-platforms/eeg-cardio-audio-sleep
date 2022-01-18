49b3ccc79c2a40fea138f50d55091962fbcc7a06
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-01-18 18:22:55 +0100

    fix F401

--- cardio_audio_sleep/__init__.py
+++ cardio_audio_sleep/__init__.py
@@ -1,3 +1,3 @@
 from ._version import __version__  # noqa: F401
 from .utils._logs import logger, set_log_level  # noqa: F401
-from .detector import Detector
+from .detector import Detector  # noqa: F401

511b598539a2a1042d894bc54e2b4fc38d224d9f
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-02-15 00:30:36 +0100

    update psychopy script

--- script/demo/sync_psychopy_sound_and_trigger.py
+++ script/demo/sync_psychopy_sound_and_trigger.py
@@ -1,16 +1,13 @@
 import time
 
-from psychopy import prefs
-prefs.hardware['audioLib'] = 'ptb'
-prefs.hardware['audioLatencyMode'] = '3'
-from psychopy import parallel, sound, core
+from bsl import StreamRecorder
+from bsl.utils.lsl import search_lsl
+from psychopy.clock import Clock
+from psychopy.sound.backend_ptb import SoundPTB as Sound
 import psychtoolbox as ptb
 
-from bsl import StreamRecorder
-from bsl.utils import Timer
-from bsl.utils.lsl import search_lsl
-
 from cardio_audio_sleep import Detector, set_log_level
+from cardio_audio_sleep.triggers.lpt import ParallelPort
 
 set_log_level('DEBUG')
 
@@ -23,16 +20,15 @@ ecg_ch_name = 'AUX7'
 recorder = StreamRecorder('/home/eeg/Downloads/', fname='test',
                           fif_subdir=False, stream_name=stream_name)
 recorder.start()
-time.sleep(0.2)
+time.sleep(0.5)
 
 #%% Init
-mySound = sound.Sound('A', secs=0.1)
-mySound.setVolume(1)
+# Create sound stimuli
+sound = Sound(value='A', secs=0.1, stereo=True, volume=1.0, blockSize=32,
+              preBuffer=-1, hamming=True)
 
 # Create trigger
-port = parallel.ParallelPort(address='/dev/parport0')
-port.setData(0)
-core.wait(0.1)
+trigger = ParallelPort(address='/dev/parport0', delay=100)
 
 # Peak detection settings
 peak_height_perc = 97.5
@@ -42,16 +38,15 @@ detector = Detector(
     stream_name, ecg_ch_name, duration_buffer=5,
     peak_height_perc=peak_height_perc, peak_prominence=peak_prominence)
 
-timer = Timer()
+timer = Clock()
+trigger_timer = Clock()
 
 #%% Main loop
 detector.prefill_buffer()
-port.setData(2)
-core.wait(0.1)
-port.setData(0)
+trigger.signal(2)
 
 timer.reset()
-while timer.sec() <= 60:
+while timer.getTime() <= 60:
     detector.update_loop()
     pos = detector.new_peaks()
     if pos is not None:
@@ -59,20 +54,19 @@ while timer.sec() <= 60:
         delay = detector.timestamps_buffer[-1] \
             - detector.timestamps_buffer[pos]
 
-        now = ptb.GetSecs()
-        mySound.play(when=now - delay + 0.05)
+        # retrieve current time and schedule sound
+        time_pre_sound = ptb.GetSecs()
+        sound.play(when=time_pre_sound - delay + 0.05)
 
-        now2 = ptb.GetSecs()
-        core.wait(now2 - now - delay + 0.05)
-
-        port.setData(1)
-        core.wait(0.1)
-        port.setData(0)
+        # retrieve current time and wait to deliver the trigger
+        target_time = ptb.GetSecs() - time_pre_sound - delay + 0.05
+        trigger_timer.reset()
+        while trigger_timer.getTime() < target_time:
+            pass
+        trigger.signal(1)
 
 time.sleep(0.2)
-port.setData(2)
-core.wait(0.1)
-port.setData(0)
+trigger.signal(2)
 
 #%% Stop Recorder
 time.sleep(0.5)

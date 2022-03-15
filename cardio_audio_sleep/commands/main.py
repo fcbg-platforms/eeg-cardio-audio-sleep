df82f73bd16f253b932c10a96ebe526af4e22a73
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-03-15 23:47:16 +0100

    add hard-coded arguments for GUI for testing purposes

--- cardio_audio_sleep/commands/main.py
+++ cardio_audio_sleep/commands/main.py
@@ -5,7 +5,13 @@ from .gui import GUI
 
 def run():
     """Entrypoint for cas <command> usage."""
+    ecg_ch_name = 'AUX7'
+    peak_height_perc = 90
+    peak_prominence = 500
+    peak_width = None
+
     app = QApplication([])
-    window = GUI()
+    window = GUI(ecg_ch_name, peak_height_perc, peak_prominence,
+                 peak_width)
     window.show()
     app.exec()

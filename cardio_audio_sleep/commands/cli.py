3f0e288c40399c1fa1143a72a83442d380c415d2
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-03-16 13:37:20 +0100

    add CLI input to provide ECG ch name and peak detection settings

---
+++ cardio_audio_sleep/commands/cli.py
@@ -0,0 +1,80 @@
+from .. import logger
+
+RETRIES = 3
+
+
+def input_ecg_ch_name():
+    """Input function for channel name (str)."""
+    msg = "[IN] Input the ECG channel name:\n>>> "
+    value = input(msg)
+    return value
+
+
+def input_peak_height_perc():
+    """Input function for peak detection settings height_perc."""
+    msg = "[IN] Input the peak height percentage parameter:\n>>> "
+    attempt = 1
+    while attempt <= RETRIES:
+        try:
+            value = float(input(msg).strip())
+            assert 0 < value < 100
+            break
+        except Exception:
+            logger.warning(
+                'The peak height percentage parameter must be a float '
+                'between 0 and 100 (%).')
+            attempt += 1
+    else:
+        raise RuntimeError('Too many erroneous answer provided.')
+
+    return value
+
+
+def input_peak_prominence():
+    """Input function for peak detection settings prominence."""
+    msg = "[IN] Input the peak prominence parameter (use None to disable):" + \
+        "\n>>> "
+    attempt = 1
+    while attempt <= RETRIES:
+        try:
+            value = input(msg)
+            if value.lower().strip() == 'none':
+                return None
+            else:
+                value = float(value.strip())
+                assert 0 < value
+                break
+        except Exception:
+            logger.warning(
+                'The peak prominence parameter must be a strictly positive '
+                'float.')
+            attempt += 1
+    else:
+        raise RuntimeError('Too many erroneous answer provided.')
+
+    return value
+
+
+def input_peak_width():
+    """Input function for peak detection settings width."""
+    msg = "[IN] Input the peak width parameter (use None to disable):" + \
+        "\n>>> "
+    attempt = 1
+    while attempt <= RETRIES:
+        try:
+            value = input(msg)
+            if value.lower().strip() == 'none':
+                return None
+            else:
+                value = float(value.strip())
+                assert 0 < value
+                break
+        except Exception:
+            logger.warning(
+                'The peak width parameter must be a strictly positive '
+                'float.')
+            attempt += 1
+    else:
+        raise RuntimeError('Too many erroneous answer provided.')
+
+    return value
--- cardio_audio_sleep/commands/main.py
+++ cardio_audio_sleep/commands/main.py
@@ -1,14 +1,16 @@
 from PyQt5.QtWidgets import QApplication
 
+from .cli import (input_ecg_ch_name, input_peak_height_perc,
+                  input_peak_prominence, input_peak_width)
 from .gui import GUI
 
 
 def run():
     """Entrypoint for cas <command> usage."""
-    ecg_ch_name = 'AUX7'
-    peak_height_perc = 90
-    peak_prominence = 500
-    peak_width = None
+    ecg_ch_name = input_ecg_ch_name()
+    peak_height_perc = input_peak_height_perc()
+    peak_prominence = input_peak_prominence()
+    peak_width = input_peak_width()
 
     app = QApplication([])
     window = GUI(ecg_ch_name, peak_height_perc, peak_prominence,

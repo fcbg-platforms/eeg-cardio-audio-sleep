ae65a102242b06bc2c1c64b5e26082887f1d8d01
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-05-25 18:36:45 +0200

    add module with visuals using opencv

---
+++ cardio_audio_sleep/visuals/__init__.py
@@ -0,0 +1,6 @@
+"""Module for visual stimulus using opencv."""
+
+from .cross import Cross  # noqa: F401
+from .text import Text  # noqa: F401
+from .filing_bar import FillingBar  # noqa: F401
+from .moving_bar import MovingBar  # noqa: F401

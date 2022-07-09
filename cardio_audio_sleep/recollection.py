5cef1af99413a623d1d2e5077b03b0c94f145e46
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-07-09 12:18:14 +0200

    simplify PsychoPy GUI

--- cardio_audio_sleep/recollection.py
+++ cardio_audio_sleep/recollection.py
@@ -13,13 +13,8 @@ def recollection():
         screen=0,
         winType='pyglet',
         allowGUI=False,
-        allowStencil=False,
         monitor=None,
-        color=[0,0,0],
-        colorSpace='rgb',
-        blendMode='avg',
-        useFBO=True,
-        units='height',
+        units='norm',
     )
     keyboard = Keyboard()
     win.callOnFlip(keyboard.clearEvents, eventType='keyboard')
@@ -28,7 +23,7 @@ def recollection():
     text_category = TextStim(
         win=win,
         text='1: percussion\n2: string\n3: wind',
-        height=0.03,
+        height=0.05,
     )
     try:
         # run routines
@@ -55,18 +50,15 @@ def _instructions(win: Window, keyboard: Keyboard):
              '- Press 2 for string\n'
              '- Press 3 for wind\n\n'
              'Press SPACE to continue.',
-        height=0.03,
+        height=0.05,
     )
-    # draw instructions
     text.setAutoDraw(True)
     win.flip()
-    # wait for 'space'
-    while True:
+    while True:  # wait for 'space'
         keys = keyboard.getKeys(keyList=['space'], waitRelease=False)
         if len(keys) != 0:
             break
         win.flip()
-    # remove instructions on next flip
     text.setAutoDraw(False)
 
 
@@ -76,23 +68,13 @@ def _fixation_cross(win: Window):
         win=win,
         vertices='cross',
         units='height',
-        size=(0.1, 0.1),
-        ori=0.0,
-        pos=(0, 0),
-        lineWidth=1.0,
-        colorSpace='rgb',
+        size=(0.05, 0.05),
         lineColor='white',
         fillColor='white',
-        opacity=None,
-        depth=0.0,
-        interpolate=True,
     )
-    # draw cross
     cross.setAutoDraw(True)
     win.flip()
-    # wait fix time
     time.sleep(2)
-    # remove cross on next flip
     cross.setAutoDraw(False)
 
 
@@ -117,48 +99,21 @@ def _confidence(win: Window):
         win=win,
         text='How confident are you?',
         pos=(0, 0.3),
-        units='norm',
-        height=0.03,
+        height=0.05,
     )
     slider = Slider(
         win=win,
         startValue=5,
-        size=(1.0, 0.1),
-        pos=(0, 0),
-        units="norm",
         labels=["low", "high"],
         ticks=(1, 2, 3, 4, 5, 6, 7, 8, 9),
         granularity=1.0,
-        style='rating',
-        styleTweaks=(),
-        opacity=None,
-        color='LightGray',
-        fillColor='Red',
-        borderColor='White',
-        colorSpace='rgb',
-        font='Open Sans',
         labelHeight=0.05,
-        flip=False,
-        depth=-1,
-        readOnly=False,
     )
     button = ButtonStim(win,
         text='Confirm',
-        font='Arvo',
-        pos=(0.5, -0.5),
-        units='norm',
+        pos=(0.5, -0.3),
         letterHeight=0.05,
         size=(0.18, 0.1),
-        borderWidth=0.0,
-        fillColor='darkgrey',
-        borderColor=None,
-        color='white',
-        colorSpace='rgb',
-        opacity=None,
-        bold=True,
-        italic=False,
-        padding=None,
-        anchor='center',
     )
     # draw components
     text.setAutoDraw(True)

8f718ce189c9e1c2c9f1349b9cd1cf0366d0fb5f
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-07-28 16:58:45 +0200

    better loading of files for the example

--- cardio_audio_sleep/example.py
+++ cardio_audio_sleep/example.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from typing import Dict
 
+import numpy as np
 from psychopy.clock import wait
 from psychopy.hardware.keyboard import Keyboard
 from psychopy.visual import ImageStim, TextStim, Window
 
-from .utils import load_instrument_images
+from .utils import load_instrument_categories, load_instrument_images
 
 
 def example(
@@ -16,10 +17,17 @@ def example(
 
     try:
         # load sounds
+        instruments = load_instrument_categories()
         assert all(len(elt) == 1 for elt in instrument_sounds.values())
-        sounds = {
-            key: Sound(path[0]) for key, path in instrument_sounds.items()
-        }
+        sounds = sorted(
+            [
+                (path[0].parent.name, Sound(path[0]))
+                for key, path in instrument_sounds.items()
+            ],
+            key=lambda x: x[0],
+        )
+        assert [elt[0] for elt in sounds] == instruments  # sanity-check
+        sounds = [elt[1] for elt in sounds]
         # prepare keyboard for interaction
         keyboard = Keyboard()
         win.callOnFlip(keyboard.clearEvents, eventType="keyboard")
@@ -30,34 +38,38 @@ def example(
             height=0.05,
             pos=(0, -0.65),
         )
-        images = load_instrument_images()
-        percussion_image = ImageStim(
-            win, images["percussion"], pos=(-0.5, -0.2)
-        )
-        string_image = ImageStim(win, images["string"], pos=(0, -0.2))
-        wind_image = ImageStim(win, images["wind"], pos=(+0.5, -0.2))
-        continue_text.setAutoDraw(True)
-        percussion_image.setAutoDraw(True)
-        string_image.setAutoDraw(True)
-        wind_image.setAutoDraw(True)
+        instrument_images = load_instrument_images()
+        assert sorted(instrument_images.keys()) == instruments  # sanity-check
+        # determine positions
+        positions = np.linspace(-0.5, 0.5, len(instruments))
+        images = list()
+        for instrument, position in zip(instruments, positions):
+            images.append(
+                ImageStim(
+                    win, instrument_images[instrument], pos=(position, 0)
+                )
+            )
+        for img in images:
+            img.setAutoDraw(True)
         # display
         win.flip()
         # start playing sounds
-        for sound in sounds.values():
+        for sound in sounds:
             sound.volume = volume  # set volume
             sound.play()
             wait(1, hogCPUperiod=0)
             sound.play()
             wait(1, hogCPUperiod=0)
+        continue_text.setAutoDraw(True)
+        win.flip()
         while True:  # wait for 'space'
             keys = keyboard.getKeys(keyList=["space"], waitRelease=False)
             if len(keys) != 0:
                 break
         # remove components
         continue_text.setAutoDraw(False)
-        percussion_image.setAutoDraw(False)
-        string_image.setAutoDraw(False)
-        wind_image.setAutoDraw(False)
+        for img in images:
+            img.setAutoDraw(False)
     except Exception:
         raise
     finally:

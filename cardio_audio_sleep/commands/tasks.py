4bc2660bc60c3514e3c244ca5c0745e988c73348
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-11-19 14:56:33 +0100

    fix commands

--- cardio_audio_sleep/commands/tasks.py
+++ cardio_audio_sleep/commands/tasks.py
@@ -9,7 +9,7 @@ from ..tasks import baseline as baseline_task
 from ..tasks import isochronous as isochronous_task
 from ..tasks import synchronous as synchronous_task
 from ..tasks._config import BASELINE_DURATION, N_OMISSION, N_SOUND
-from ._utils import ch_name_ecg, fq_deviant, fq_target, stream, verbose
+from ._utils import ch_name_ecg, stream, verbose
 
 
 @click.command()
@@ -34,13 +34,11 @@ def baseline(duration: float, verbose: str) -> None:
     help="Delay between 2 stimulus in seconds.",
     type=float,
 )
-@fq_target
-@fq_deviant
 @verbose
-def isochronous(delay: float, target: float, deviant: float, verbose: str) -> None:
+def isochronous(delay: float, verbose: str) -> None:
     """Run an isochronous task."""
     set_log_level(verbose)
-    isochronous_task(delay, target=target, deviant=deviant)
+    isochronous_task(delay)
 
 
 @click.command()
@@ -51,12 +49,8 @@ def isochronous(delay: float, target: float, deviant: float, verbose: str) -> No
     default=(0.5, 1.5),
     show_default=True,
 )
-@fq_target
-@fq_deviant
 @verbose
-def asynchronous(
-    delays: tuple[float, float], target: float, deviant: float, verbose: str
-) -> None:
+def asynchronous(delays: tuple[float, float], verbose: str) -> None:
     """Run an asynchronous task."""
     set_log_level(verbose)
     # create random peak position based on the min/max delays requested
@@ -67,38 +61,14 @@ def asynchronous(
     rng = np.random.default_rng()
     delays = rng.uniform(low=delays[0], high=delays[1], size=N_SOUND + N_OMISSION - 1)
     peaks = np.hstack(([0], np.cumsum(delays)))
-    asynchronous_task(peaks, target=target, deviant=deviant)
+    asynchronous_task(peaks)
 
 
 @click.command()
 @stream
 @ch_name_ecg
-@click.option(
-    "--delays",
-    help="Min and max delays between 2 stimuli in seconds.",
-    type=(float, float),
-    default=(0.5, 1.5),
-    show_default=True,
-)
-@fq_target
-@fq_deviant
 @verbose
-def synchronous(
-    stream: str,
-    ch_name_ecg: str,
-    delays: tuple[float, float],
-    target: float,
-    deviant: float,
-    verbose: str,
-) -> None:
-    """Run a synchronous cardiac task."""
+def synchronous(stream: str, ch_name_ecg: str, verbose: str) -> None:
+    """Run a synchronous respiration task."""
     set_log_level(verbose)
-    # create random peak position based on the min/max delays requested
-    if delays[0] <= 0:
-        raise ValueError("The minimum delay must be strictly positive.")
-    if delays[1] <= 0:
-        raise ValueError("The maximum delay must be strictly positive.")
-    rng = np.random.default_rng()
-    delays = rng.uniform(low=delays[0], high=delays[1], size=N_SOUND + N_OMISSION - 1)
-    peaks = np.hstack(([0], np.cumsum(delays)))
-    synchronous_task(stream, ch_name_ecg, peaks, target=target, deviant=deviant)
+    synchronous_task(stream, ch_name_ecg)

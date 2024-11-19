51adacb1fff30b562469d38f55620533900c75a4
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-11-19 13:41:07 +0100

    simplify viz module

--- cardio_audio_sleep/viz.py
+++ cardio_audio_sleep/viz.py
@@ -20,32 +20,23 @@ class Viewer:
     Parameters
     ----------
     %(ecg_ch_name)s
-    ecg_height : float | None
+    ecg_height : float
         The height of the ECG peaks as a percentage of the data range, between 0 and 1.
     """
 
-    def __init__(
-        self,
-        ecg_ch_name: str | None,
-        ecg_height: float | None,
-    ) -> None:
+    def __init__(self, ecg_ch_name: str, ecg_height: float) -> None:
         if plt.get_backend() != "QtAgg":
             plt.switch_backend("QtAgg")
         if not plt.isinteractive():
             plt.ion()  # enable interactive mode
-        picks = [elt for elt in (ecg_ch_name,) if elt is not None]
-        self._fig, axes = plt.subplots(len(picks), 1, figsize=(8, 8 * len(picks)))
-        if ecg_ch_name is not None:
-            self._axes = {"ecg": axes}
-            axes.set_title(f"ECG: {ecg_ch_name}")
-        self._peaks = {"ecg": []}
+        self._fig, self._axes = plt.subplots(1, 1, figsize=(8, 8))
+        self._axes.set_title(f"ECG: {ecg_ch_name}")
+        self._peaks = []
         self._ecg_height = ecg_height
         plt.show()
 
     @fill_doc
-    def plot(
-        self, ts: NDArray[np.float64], data: NDArray[np.float64], ch_type: str
-    ) -> None:
+    def plot(self, ts: NDArray[np.float64], data: NDArray[np.float64]) -> None:
         """Plot the respiration or cardiac data and peaks.
 
         Parameters
@@ -54,43 +45,37 @@ class Viewer:
             Timestamps of the respiration data.
         data : array of shape (n_samples,)
             Respiration or cardiac data.
-        %(ch_type)s
         """
         assert ts.ndim == 1
         assert data.ndim == 1
         # prune peaks outside of the viewing window
-        for k, peak in enumerate(self._peaks[ch_type]):
+        for k, peak in enumerate(self._peaks):
             if ts[0] <= peak:
                 idx = k
                 break
         else:
             idx = 0
-        self._peaks[ch_type] = self._peaks[ch_type][idx:]
+        self._peaks = self._peaks[idx:]
         # update plotting window
-        self._axes[ch_type].clear()
-        self._axes[ch_type].plot(ts, data)
-        for peak in self._peaks[ch_type]:
-            self._axes[ch_type].axvline(peak, color="red", linestyle="--")
-        if ch_type == "ecg":
-            assert self._ecg_height is not None  # sanity-check
-            self._axes[ch_type].axhline(
-                np.percentile(data, self._ecg_height * 100),
-                color="green",
-                linestyle="--",
-            )
+        self._axes.clear()
+        self._axes.plot(ts, data)
+        for peak in self._peaks:
+            self._axes.axvline(peak, color="red", linestyle="--")
+        self._axes.axhline(
+            np.percentile(data, self._ecg_height * 100), color="green", linestyle="--"
+        )
         self._fig.canvas.draw()
         self._fig.canvas.flush_events()
 
     @fill_doc
-    def add_peak(self, peak: float, ch_type: str) -> None:
+    def add_peak(self, peak: float) -> None:
         """Add a peak to the viewer.
 
         Parameters
         ----------
         peak : float
             Timestamp of the peak.
-        %(ch_type)s
         """
         check_type(peak, ("numeric",), "peak")
         assert 0 < peak
-        self._peaks[ch_type].append(peak)
+        self._peaks.append(peak)
